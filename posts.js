3351a6a697beaf3fae267a0d1213c4b61a8bdeb2
mhneloy <mhneloy708@gmail.com>
2026-04-08 00:57:15 +0600

    module 4-7 restart completed

--- posts.js
+++ posts.js
@@ -1,13 +1,16 @@
 const loadPost = () => {
-  const url = "https://jsonplaceholder.typicode.com/posts";
-  fetch(url)
+  const url1 = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url1)
     .then((res) => res.json())
     .then((data) => {
       displayPosts(data);
     });
 };
 
-const displayPosts = (posts) => {
+const displayPosts = async (posts) => {
+  const url2 = "https://jsonplaceholder.typicode.com/todos";
+  const todos = await fetch(url2);
+  const todosData = await todos.json();
   // 1. get the container
   const post_container = document.getElementById("post-container");
   post_container.innerHTML = "";
@@ -20,7 +23,15 @@ const displayPosts = (posts) => {
           <h2>${post.title}</h2>
           <p>${post.body}}${count}</p>
   `;
-    post_container.append(postCard);
+    post_container.appendChild(postCard);
+  });
+  todosData.forEach((todo) => {
+    const h4 = document.createElement("h4");
+    h4.innerHTML = `
+    ${todo.title}
+    ${todo.completed === true ? `<i class="fa-solid fa-circle-check"></i>` : ``}`;
+    post_container.appendChild(h4);
+    console.log(h4);
   });
 };
 
